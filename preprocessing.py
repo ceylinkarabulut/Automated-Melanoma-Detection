d89ecad28d4bd7edaa3da12b91e11354f756ff3b
idilkizilcelik <mariaidilka13@icloud.com>
2026-07-31 13:55:07 +0300

    feat: preprocessing.py - rescale_image ve extract_patches implementasyonu

--- preprocessing.py
+++ preprocessing.py
@@ -2,14 +2,46 @@
 
 import cv2
 import numpy as np
+from numpy.lib.stride_tricks import sliding_window_view
 
 
 def rescale_image(img, long_side, is_mask=False):
-    """Uzun kenarı long_side yap, en-boy oranını koru.
-    is_mask=True -> INTER_NEAREST"""
-    pass
+    H, W = img.shape[:2]
+    if H>W:
+        scale= long_side/H
+    else:
+        scale= long_side/W
+
+    new_H = int(round(scale * H))
+    new_W = int(round(scale * W))
+    if is_mask:
+     interpolation= cv2.INTER_NEAREST
+    else:
+     interpolation= cv2.INTER_LINEAR
+
+
+    resized=  cv2.resize(img,( new_W, new_H), interpolation= interpolation)
+    return resized
+
 
 
 def extract_patches(img, mask, n, stride, threshold):
     """Kayan pencere + maske filtresi -> [patch, ...]  her biri (n,n,3)"""
-    pass
+    windows= sliding_window_view(mask, (n,n))
+    sampled = windows[::stride, ::stride]
+    img_windows= sliding_window_view(img, (n,n), axis=(0,1))
+    img_sampled= img_windows[::stride, ::stride]
+    img_sampled = np.moveaxis(img_sampled, 2, -1)
+    ratios = np.mean(sampled == 255, axis=(2, 3))
+
+    keep = ratios >= threshold
+
+
+    selected = img_sampled[keep]
+
+
+
+
+    patches = [p.astype(np.uint8) for p in selected]
+    return patches
+
