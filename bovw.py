095f2d18d4bbb8dcc52d46d7d0cc562a33f47d31
CookieMonz1er <cengizkutay134@gmail.com>
2026-07-31 17:59:04 +0300

    bovw.py: add random_state=config.SEED to MiniBatchKMeans for reproducible cluster centers

--- bovw.py
+++ bovw.py
@@ -1,6 +1,7 @@
 """Weighted Bag of Visual Words: sözlük ve histogram."""
 
 import numpy as np
+import config
 from sklearn.cluster import MiniBatchKMeans
 from scipy.spatial.distance import cdist
 
@@ -8,9 +9,9 @@ from scipy.spatial.distance import cdist
 def train_dictionary(features_list, labels, k):
     benign_features = features_list[labels == 0]
     malignant_features = features_list[labels == 1]
-    kmeans_benign = MiniBatchKMeans(n_clusters=k // 2)
+    kmeans_benign = MiniBatchKMeans(n_clusters=k // 2, random_state=config.SEED)
     kmeans_benign.fit(benign_features)
-    kmeans_malignant = MiniBatchKMeans(n_clusters=k // 2)
+    kmeans_malignant = MiniBatchKMeans(n_clusters=k // 2, random_state=config.SEED)
     kmeans_malignant.fit(malignant_features)
     centers = np.vstack([kmeans_benign.cluster_centers_, kmeans_malignant.cluster_centers_])
     return centers
